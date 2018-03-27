874006c17704a2621087cb9613df2a19c1f5c565
Vedran <vedranivic777@gmail.com>
2018-03-27 23:16:56 +0200

    Popravlje XML layout (greška kod dimens.xml i vrijednosti dimenzija te veličina slika - bile su prevelike). Slike zamjenjene manjim .png ekvivalentima. Omogućen ButterKnife i dodana mogućnost 'skrolanja' teksta te citati u strings.xml kao niz stringova.

--- app/src/main/java/hr/ferit/vedran/inspired/MainActivity.java
+++ app/src/main/java/hr/ferit/vedran/inspired/MainActivity.java
@@ -2,12 +2,40 @@ package hr.ferit.vedran.inspired;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tvTuring)
+    TextView tvTuring;
+    @BindView(R.id.tvJobs)
+    TextView tvJobs;
+    @BindView(R.id.tvGates)
+    TextView tvGates;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        tvTuring.setMovementMethod(new ScrollingMovementMethod());
+        tvJobs.setMovementMethod(new ScrollingMovementMethod());
+        tvGates.setMovementMethod(new ScrollingMovementMethod());
+
+    }
+
+    private void displayToastMessage(String Text) {
+        Toast T = Toast.makeText(MainActivity.this, Text, Toast.LENGTH_LONG+1);
+        T.show();
     }
 }
